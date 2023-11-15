718eb2a9a4d9ddea73257c73fdf4725fc56f976d
주윤 <joo6881@naver.com>
2023-11-15 12:33:19 +0900

    initial commit.

---
+++ src/test/java/com/houselink/HouseLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houselink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
